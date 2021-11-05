2a4c560b42787d9aabc68715dee4fa0b92663e09
Misha Miroshnikov <41021818+mmiroshnikov@users.noreply.github.com>
2021-11-04 18:30:19 -0700

    Init

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const http = require("http");
+const url = require("url");
+const openurl = require('openurl');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+http
+  .createServer(function(req, res) {
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
+
+    function parseJwt(token) {
+      var base64Url = token.split(".")[1];
+      console.log(base64Url);
+      return Buffer.from(base64Url, "base64").toString();
+    }
+
+    let whiteListed = [
+      'harley.homewood@team17.com',
+      'lakhvir.takhar@team17.com',
+      'mich.davis@team17.com',
+      'phil.harper@team17.com',
+      'rossfairhurst17@gmail.com',
+      'stephan@weareframework.co.uk',
+      'sam.bishop@team17.com',
+      'mikoza@gmail.com',
+    ]
+
+    checkEmail = (e) => {
+
+      var email = e.split('@')[0].replace('.', '') + '@' + e.split('@')[1];
+
+      if (email.split("@").length === 2 && email.split("@")[1] === "xsolla.com") return true;
+
+      if (whiteListed.filter(one=> one === email.toLowerCase()).length === 1) return true
+    }
+
+
+    if (Object.keys(queryObject).length) {
+      var params = parseJwt(queryObject["token"]);
+      console.log(params);
+      console.log(queryObject["token"]);
+
+      var email = JSON.parse(params)["email"];
+
+      if (checkEmail(email)) {
+        res.writeHead(301,
+          {Location: `https://xsolla-game-store.netlify.app/?APIKEY=key2e5vqPmfmASari&USER=${email}`}
+        );
+        res.end(`${ email} is authorized`);
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html"});
+        res.end(`${email} is NOT authorized`);
+      }
+    } else {
+      res.writeHead(301,
+        {Location: `https://xsolla-game-store.netlify.app/logout`}
+      );
+      res.end("Unauthorized");
+    }
+  })
+  .listen(process.env.PORT || 8080);
